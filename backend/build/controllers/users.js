cdf6e606dee294469943ce1b8a6a5a9addbfc6c3
Miguel-Ubuntu-lap <mgarlcr@gmail.com>
2022-06-29 18:06:42 -0400

    Moved dependencies from from dev

--- backend/build/controllers/users.js
+++ backend/build/controllers/users.js
@@ -12,12 +12,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.users = void 0;
 const express_1 = __importDefault(require("express"));
 const users = express_1.default.Router();
-exports.users = users;
 users.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res.status(200).json({
         message: 'You are at users/'
     });
 }));
+exports.default = users;
--- backend/build/controllers/vehicles.js
+++ backend/build/controllers/vehicles.js
@@ -12,12 +12,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.vehicles = void 0;
 const express_1 = __importDefault(require("express"));
 const vehicles = express_1.default.Router();
-exports.vehicles = vehicles;
 vehicles.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res.status(200).json({
         message: 'You are at vehicles/'
     });
 }));
+exports.default = vehicles;
