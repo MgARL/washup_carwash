2f34c9a3299da2043702f0c9c7439ea321d128a2
Miguel-Ubuntu-lap <mgarlcr@gmail.com>
2023-02-27 21:19:19 -0500

    Updated Prod DB Envs

--- backend/config/config.js
+++ backend/config/config.js
@@ -16,11 +16,11 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
+    "username": process.env.PGUSER,
+    "password": process.env.PGPASSWORD,
+    "database": process.env.PGDATABASE,
+    "host": process.env.PGHOST,
+    "port": process.env.PGPORT,
     "dialect": "postgres"
   }
 }
